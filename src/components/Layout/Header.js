5d727d3548670e9cfe361f022836463e9882e43d
man9936 <mk9934dude@gmail.com>
2022-11-18 19:56:14 +0530

    first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <React.Fragment>
+
+<div>
+    <img href='.../Assets/meals.jpg'/>
+</div>
+    </React.Fragment>
+    
+  )
+}
+
+export default Header;
